b4d6feac3f849cfc1eafd8d50be9dfdb5c8dca7e
Yu Wu <yolandawu131@gmail.com>
2023-07-18 17:56:19 +0800

    add sample(#4662)
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>

---
+++ python/fate_client/pipeline/components/fate/sample.py
@@ -0,0 +1,47 @@
+#
+#  Copyright 2023 The FATE Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from typing import List
+
+from ..component_base import Component
+from ...conf.types import PlaceHolder
+from ...interface import ArtifactType
+
+
+class sample(Component):
+    yaml_define_path = "./component_define/fate/sample.yaml"
+
+    def __init__(self,
+                 _name: str,
+                 runtime_roles: List[str] = None,
+                 mode: str = "random",
+                 replace: bool = False,
+                 frac: float = None,
+                 n: int = None,
+                 random_state: int = None,
+                 ctx_mode: str = "hetero",
+                 input_data: ArtifactType = PlaceHolder()
+                 ):
+        inputs = locals()
+        self._process_init_inputs(inputs)
+        super(sample, self).__init__()
+        self._name = _name
+        self.runtime_roles = runtime_roles
+        self.mode = mode
+        self.replace = replace
+        self.frac = frac
+        self.n = n
+        self.random_state = random_state
+        self.ctx_mode = ctx_mode
+        self.input_data = input_data
