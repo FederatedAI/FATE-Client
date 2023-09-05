151b999c8c809e4bbbbfc5f0298f24ba16cf6a69
Yu Wu <yolandawu131@gmail.com>
2023-09-05 18:46:29 +0800

    coordinated lr & linr add key length & floating point precision param(#4659) binning add key length param(#4660) edit fate test config(#5008) fix pipeline examples
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>

--- python/fate_client/pipeline/components/fate/coordinated_linr.py
+++ python/fate_client/pipeline/components/fate/coordinated_linr.py
@@ -36,6 +36,8 @@ class CoordinatedLinR(Component):
                  cv_data: ArtifactType = PlaceHolder(),
                  output_cv_data: bool = True,
                  cv_param: dict = PlaceHolder(),
+                 floating_point_precision: int = 23,
+                 key_length: int = 1024,
                  validate_data: ArtifactType = PlaceHolder(),
                  test_data: ArtifactType = PlaceHolder(),
                  input_model: ArtifactType = PlaceHolder(),
@@ -62,3 +64,5 @@ class CoordinatedLinR(Component):
         self.cv_param = cv_param
         self.output_cv_data = output_cv_data
         self.warm_start_model = warm_start_model
+        self.floating_point_precision = floating_point_precision
+        self.key_length = key_length
