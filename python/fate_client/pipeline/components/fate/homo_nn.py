5ca5a793d2f744fc07733c33cf063c7a2382aaed
cwj <talkingwallace@sohu.com>
2023-09-11 11:45:34 +0800

    rename fate_client
    Signed-off-by: cwj <talkingwallace@sohu.com>

--- python/fate_client/pipeline/components/fate/homo_nn.py
+++ python/fate_client/pipeline/components/fate/homo_nn.py
@@ -15,7 +15,7 @@
 from typing import List
 from fate_client.pipeline.conf.types import PlaceHolder
 from fate_client.pipeline.components.fate.nn.loader import Loader, ModelLoader, CustFuncLoader, DatasetLoader
-from fate_client.pipeline.components.fate.nn.patched_torch.base import PatchedTorchModule, PatchedTorchOptimizer, Sequential
+from fate_client.pipeline.components.fate.nn.torch.base import TorchModule, TorchOptimizer, Sequential
 from typing import Union
 from fate_client.pipeline.components.fate.nn.algo_params import TrainingArguments, FedArguments
 from typing import Literal
@@ -26,9 +26,9 @@ from ...interface import ArtifactType
 
 def get_config_of_default_runner(
         algo: str = 'fedavg',
-        model: Union[PatchedTorchModule, Sequential, ModelLoader] = None,
-        optimizer: Union[PatchedTorchOptimizer, Loader] = None,
-        loss: Union[PatchedTorchModule, CustFuncLoader] = None,
+        model: Union[TorchModule, Sequential, ModelLoader] = None,
+        optimizer: Union[TorchOptimizer, Loader] = None,
+        loss: Union[TorchModule, CustFuncLoader] = None,
         training_args: TrainingArguments = None,
         fed_args: FedArguments = None,
         dataset: DatasetLoader = None,
@@ -37,14 +37,14 @@ def get_config_of_default_runner(
         task_type: Literal['binary', 'multi', 'regression', 'others'] = 'binary'
     ):
         
-    if model is not None and not isinstance(model, (PatchedTorchModule, Sequential, ModelLoader)):
-        raise ValueError(f'The model is of type {type(model)}, not PatchedTorchModule, Sequential, or ModelLoader. Remember to use patched_torch_hook for passing NN Modules or Optimizers.')
+    if model is not None and not isinstance(model, (TorchModule, Sequential, ModelLoader)):
+        raise ValueError(f'The model is of type {type(model)}, not TorchModule, Sequential, or ModelLoader. Remember to use patched_torch_hook for passing NN Modules or Optimizers.')
     
-    if optimizer is not None and not isinstance(optimizer, (PatchedTorchOptimizer, Loader)):
-        raise ValueError(f'The optimizer is of type {type(optimizer)}, not PatchedTorchOptimizer or Loader. Remember to use patched_torch_hook for passing NN Modules or Optimizers.')
+    if optimizer is not None and not isinstance(optimizer, (TorchOptimizer, Loader)):
+        raise ValueError(f'The optimizer is of type {type(optimizer)}, not TorchOptimizer or Loader. Remember to use patched_torch_hook for passing NN Modules or Optimizers.')
     
-    if loss is not None and not isinstance(loss, (PatchedTorchModule, CustFuncLoader)):
-        raise ValueError(f'The loss function is of type {type(loss)}, not PatchedTorchModule or CustFuncLoader.')
+    if loss is not None and not isinstance(loss, (TorchModule, CustFuncLoader)):
+        raise ValueError(f'The loss function is of type {type(loss)}, not TorchModule or CustFuncLoader.')
     
     if training_args is not None and not isinstance(training_args, TrainingArguments):
         raise ValueError(f'Training arguments are of type {type(training_args)}, not TrainingArguments.')
